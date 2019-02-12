d246def6eaed1edb88ffc3aeeafcc3830f011eec
sayerwalm <sayerwalam@gmail.com>
2019-02-12 12:17:35 -0800

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  	root 'user#index'
+
+	# standard GET requests
+	get 'user/index'
+
+	# all other GET requests
+	match '/logout' => 'user#logout', :via => :get
+	match '/home' => 'user#home', :via => :get
+
+	# all POST requests
+    match '/signup' => 'user#create', :via => :post
+    match '/login' => 'user#login', :via => :post
+end
